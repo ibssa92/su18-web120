6cb367c09d6e82369c26f1112da6d5b579f86278
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2018-07-24 17:48:14 -0700

    Add files via upload

---
+++ web120/index.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php"?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/ibssa.jpg" class="desktop" alt="ibssa" />
+ <img src="images/champs.png" class="phone" alt="champs" />
+ <p> My name is Ibssa Wado. I was born and raised in the mean streets of Bridgeport,CT. My father is from Oromia and my mother is half Somali and half Eritrean, so often times I just tell people I’m East African to keep things short and simple. I am also Muslim. I moved from Bridgeport to Seattle,WA in 2005. I have 2 siblings; an older brother and a younger brother. A few of my hobbies include playing basketball, football, watching sports, travelling the world, and spending quality time with family and friends. I am also somewhat of a Sneakerhead.</p>
+ 
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+
+ <img src="images/group.png" class="tablet" alt="group" />
+ <p>I completed my bachelor’s degree in Media and Communication Studies from the University of Washington (Bothell) in June 2015. After graduating, I took some time off to relax and travel a bit. I started working at Amazon in 2017 as a Seller Support Associate. After a year of that, I decided it would be in my best interest to return to school and build some tech skills. I am currently pursuing a Programming Certificate at Seattle Central College, where I am studying Web and Database Development.</p>
+ 
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php"?>
